d0cc583742108761345c5fd1dfa814a1e3466c58
NoahPeeters <noah.peeters@icloud.com>
2017-09-11 00:59:08 +0200

    Initial Commit

---
+++ Document Importer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Document Importer
+//
+//  Created by Noah Peeters on 11.09.17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Document Importer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Document Importer
+//
+//  Created by Noah Peeters on 11.09.17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
